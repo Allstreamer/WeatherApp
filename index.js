90402b5102724651a47e19136783901b7482c5ee
Allstreamer <48365544+Allstreamer@users.noreply.github.com>
2022-04-19 20:16:06 +0200

    uhh

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-$.getJSON("https://api.openweathermap.org/data/2.5/weather?lat=53.6477&lon=10.1701&appid=62f8349b2b00a2dc7db78b2c93e8095f", function( data ) {
+$.getJSON("https://api.openweathermap.org/data/2.5/weather?lat=52.6477&lon=10.5701&appid=62f8349b2b00a2dc7db78b2c93e8095f", function( data ) {
 	document.getElementById("location").innerHTML = data["name"];
-	document.getElementById("lat").innerHTML = `lat: ${data["coord"]["lat"]}`;
-	document.getElementById("lon").innerHTML = `lon: ${data["coord"]["lon"]}`;
 	
 });
